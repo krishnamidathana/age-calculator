f30c50b298b6c34a6e8ec4484323cadfdbe59ca5
KRISHNA MOHAN <134590019+krishnamidathana@users.noreply.github.com>
2023-07-05 20:51:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+const getAge = document.getElementById("calculate");
+var todaydate = new Date();
+var d2 = new Date().getDate();
+var m2 = new Date().getMonth() + 1;
+var y2 = new Date().getFullYear();
+
+var maxDate = y2 + "-" + m2 + "-" + d2;
+var userInput = document.getElementById("demo");
+userInput.setAttribute("max", maxDate);
+
+getAge.addEventListener("click", calculateAge);
+function calculateAge() {
+  let userDate = new Date(userInput.value);
+
+  let d1 = userDate.getDate();
+  let m1 = userDate.getMonth() + 1;
+  let y1 = userDate.getFullYear();
+
+  var yCount = document.getElementById("yCount");
+  var mCount = document.getElementById("mCount");
+  var dCount = document.getElementById("dCount");
+
+  var d3, m3, y3;
+
+  y3 = y2 - y1;
+
+  if (m2 >= m1) {
+    m3 = m2 - m1;
+  } else {
+    y3--;
+    m3 = 12 + m2 - m1;
+  }
+
+  if (d2 >= d1) {
+    d3 = d2 - d1;
+  } else {
+    m3--;
+    d3 = daysInMonth(y1, m1) + d2 - d1;
+  }
+  if (m3 < 0) {
+    m3 = 11;
+    y3--;
+  }
+
+  if (m3 < 10) {
+    m3 = "0" + m3;
+  }
+  if (d3 < 10) {
+    d3 = "0" + d3;
+  }
+
+  function daysInMonth(year, month) {
+    return new Date(year, month, 0).getDate();
+  }
+
+  yCount.innerText = y3;
+  mCount.innerText = m3;
+  dCount.innerText = d3;
+  console.log(yCount, mCount, dCount);
+  console.log(y3, m3, d3);
+  userInput.value = "";
+}
